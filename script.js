65525346b688f8557ec7970d82604c185a57e87a
sunnykushwahathakur <sunnykushwahathakur@gmail.com>
2024-09-18 02:27:06 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+    
 var tl = gsap.timeline()
 tl.from(".line h1", {
     y: 150,
@@ -29,15 +30,35 @@ tl.to(".line h2", {
 tl.to("#loader",{
     opacity: 0,
     duration: 0.2,
-    delay: 4
+    delay: 0
 }) 
 tl.from("#page1",{
     delay:0.2,
     y: 1600,
     opacity:0,
-    duraiton: 0.5,
+    duraiton: 0.6,
     ease: Power4
 })
 tl.to("#loader",{
     display: "none"
 })
+tl.from("#nav", {
+    opacity: 0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y: 120,
+    stagger: 0.2,
+})
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation(); 
+cursorAnimation()
